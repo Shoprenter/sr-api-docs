fb9538f2433dd0604ed363b7abe8366a88c825f6
László Szabó <laszlo.szabo@shoprenter.hu>
2025-07-09 13:09:54 +0200

    SPP-5804

---
+++ .vuepress/components/ResourceScopes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="data">
+    <h2>Required Scopes</h2>
+    <p>Only applies to <a>JWT-authenticated requests</a>. See the <a href="/development/api/10_scopes.html">full list of endpoint scopes</a>.</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Method</th>
+          <th>Scope</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(scope, index) in data[resource].methods" :key="index">
+          <td>{{index}}</td>
+          <td v-html="scope"></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      data: null
+    }
+  },
+  mounted() {
+    this.data = require('../../fixtures/scopes/scopes.json');
+  },
+  props: {
+    resource: {
+      type: String,
+      required: true,
+    }
+  },
+}
+</script>
